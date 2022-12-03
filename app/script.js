aa91bb4e4269909e5d190a77f29df5842f4c8833
umar khan <umar.khan@student.uts.edu.au>
2022-12-04 02:10:34 +1100

    added animations to hamburger

--- app/script.js
+++ app/script.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+const hamburger = document.querySelector("#hamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+hamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
