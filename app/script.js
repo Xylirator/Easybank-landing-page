d7644334e11e9ef62714ee65ec9e27f7768b2746
umar khan <umar.khan@student.uts.edu.au>
2022-12-04 17:25:41 +1100

    finished animated mobile menu

--- app/script.js
+++ app/script.js
@@ -1,17 +1,26 @@
 const hamburger = document.querySelector("#hamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fades = document.querySelectorAll(".has__fade")
+const body = document.querySelector("body")
 
 hamburger.addEventListener("click", function () {
   console.log("click hamburger");
 
   if (header.classList.contains("open")) {
+    body.classList.remove('noscroll')
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
-  } else {
+    fades.forEach(function(element){
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    })
+  } 
+  else {
+    body.classList.add('noscroll')
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fades.forEach(function(element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    })
   }
 });
